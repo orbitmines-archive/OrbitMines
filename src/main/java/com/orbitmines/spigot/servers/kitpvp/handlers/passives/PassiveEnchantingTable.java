5aa0638166d089d7ed5c91d2e1d06c5862660267
Robin Egberts <playwarrior2@gmail.com>
2018-10-28 19:50:56 +0100

    Enchanter Kit added

--- src/main/java/com/orbitmines/spigot/servers/kitpvp/handlers/passives/PassiveEnchantingTable.java
+++ src/main/java/com/orbitmines/spigot/servers/kitpvp/handlers/passives/PassiveEnchantingTable.java
@@ -1,14 +1,12 @@
 package com.orbitmines.spigot.servers.kitpvp.handlers.passives;
 
 import com.orbitmines.api.utils.RandomUtils;
-import com.orbitmines.spigot.api.handlers.itembuilders.ItemBuilder;
-import com.orbitmines.spigot.api.handlers.kit.Kit;
-import com.orbitmines.spigot.servers.kitpvp.handlers.KitPvPPlayer;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class PassiveEnchantingTable implements Passive.Handler<PlayerDeathEvent> {
 
@@ -23,14 +21,10 @@ public class PassiveEnchantingTable implements Passive.Handler<PlayerDeathEvent>
         Player entity = event.getEntity();
         Player kill = entity.getKiller();
 
-        KitPvPPlayer killer = KitPvPPlayer.getPlayer(kill);
-        Kit kit = killer.getSelectedKit().getKit();
-
         int slot = enchantedArmor(level) ? RandomUtils.i(0, 4) : 0;
 
-        System.out.println(slot);
-
-        ItemBuilder item = getRandomBuilder(kit, slot);
+        ItemStack item = getRandomBuilder(kill.getInventory(), slot);
+        ItemMeta meta = item.getItemMeta();
 
         Enchantment[] enchantments = slot == 0 ? attack_enchantments : protect_enchantments;
 
@@ -40,29 +34,28 @@ public class PassiveEnchantingTable implements Passive.Handler<PlayerDeathEvent>
         int tries = 0;
 
         while ((l == -1 || enchantment.getMaxLevel() <= l) && tries <= 10) {
-            enchantment = RandomUtils.randomFrom(enchantment);
-            l = item.getEnchantments().getOrDefault(enchantment, 0) + 1;
+            enchantment = RandomUtils.randomFrom(enchantments);
+            l = meta.getEnchantLevel(enchantment) + 1;
             tries++;
-            System.out.println(enchantment.getKey().getKey() + ":" + l);
         }
 
-        item.getEnchantments().put(enchantment, l);
+        item.addEnchantment(enchantment, l);
 
-        this.setItem(kill, slot, item.build());
+        this.setItem(kill, slot, item);
     }
 
-    private ItemBuilder getRandomBuilder(Kit kit, int slot) {
+    private ItemStack getRandomBuilder(PlayerInventory inventory, int slot) {
         switch (slot) {
             case 0:
-                return kit.getFirstItem().clone();
+                return inventory.getItemInMainHand();
             case 1:
-                return kit.getHelmet().clone();
+                return inventory.getHelmet();
             case 2:
-                return kit.getChestplate().clone();
+                return inventory.getChestplate();
             case 3:
-                return kit.getLeggings().clone();
+                return inventory.getLeggings();
             case 4:
-                return kit.getBoots().clone();
+                return inventory.getBoots();
         }
         return null;
     }
